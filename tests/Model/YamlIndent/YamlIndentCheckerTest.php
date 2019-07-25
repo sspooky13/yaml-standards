48149b2c82364bbdfa36c1276103a293371944c4
Petr Heinz <petr.heinz@shopsys.com>
2019-07-25 17:58:56 +0200

    add failing unit test for indentation edge-case
    - see https://github.com/shopsys/shopsys/pull/539#issuecomment-515050285

--- tests/Model/YamlIndent/YamlIndentCheckerTest.php
+++ tests/Model/YamlIndent/YamlIndentCheckerTest.php
@@ -24,6 +24,7 @@ class YamlIndentCheckerTest extends TestCase
     {
         $inputSettingData = $this->getInputSettingDataMock();
         $pathToFiles = [
+            __DIR__ . '/resource/unfixed/kustomization.yaml',
             __DIR__ . '/resource/unfixed/shopsys-service.yml',
             __DIR__ . '/resource/unfixed/symfony-config.yml',
             __DIR__ . '/resource/unfixed/symfony-route.yml',
@@ -54,6 +55,7 @@ class YamlIndentCheckerTest extends TestCase
     {
         $inputSettingData = $this->getInputSettingDataMock();
         $pathToFiles = [
+            __DIR__ . '/resource/fixed/kustomization.yaml',
             __DIR__ . '/resource/fixed/shopsys-service.yml',
             __DIR__ . '/resource/fixed/symfony-config.yml',
             __DIR__ . '/resource/fixed/symfony-route.yml',
