e312a188655a9421fecf5c28cb4d5d82cee46f56
Peter Turek <peter.turek13@gmail.com>
2019-12-07 13:03:31 +0100

    create new standard: YamlServiceAliasing

---
+++ src/Model/YamlServiceAliasing/YamlServiceAliasingChecker.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace YamlStandards\Model\YamlServiceAliasing;
+
+use SebastianBergmann\Diff\Differ;
+use YamlStandards\Command\ProcessOutput;
+use YamlStandards\Model\CheckerInterface;
+use YamlStandards\Model\Component\YamlService;
+use YamlStandards\Model\Config\StandardParametersData;
+use YamlStandards\Result\Result;
+
+/**
+ * Check service file observe uniform aliasing
+ */
+class YamlServiceAliasingChecker implements CheckerInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function check(string $pathToYamlFile, StandardParametersData $standardParametersData): Result
+    {
+        $yamlContent = file_get_contents($pathToYamlFile);
+        $yamlContent = str_replace("\r", '', $yamlContent); // remove carriage returns
+        $yamlLines = explode("\n", $yamlContent);
+
+        if (YamlServiceAliasingDataFactory::existsServicesInHighestParent($yamlLines) === false) {
+            return new Result($pathToYamlFile, Result::RESULT_CODE_OK, ProcessOutput::STATUS_CODE_OK);
+        }
+
+        $correctYamlLines = YamlServiceAliasingDataFactory::getCorrectYamlLines($yamlLines, YamlService::getYamlData($pathToYamlFile), $standardParametersData);
+        $correctYamlContent = implode("\n", $correctYamlLines);
+
+        if ($yamlContent === $correctYamlContent) {
+            return new Result($pathToYamlFile, Result::RESULT_CODE_OK, ProcessOutput::STATUS_CODE_OK);
+        }
+
+        $differ = new Differ();
+        $diffBetweenStrings = $differ->diff($yamlContent, $correctYamlContent);
+
+        return new Result($pathToYamlFile, Result::RESULT_CODE_INVALID_FILE_SYNTAX, ProcessOutput::STATUS_CODE_INVALID_FILE_SYNTAX, $diffBetweenStrings, true);
+    }
+}
---
+++ src/Model/YamlServiceAliasing/YamlServiceAliasingFixer.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace YamlStandards\Model\YamlServiceAliasing;
+
+use SebastianBergmann\Diff\Differ;
+use YamlStandards\Command\ProcessOutput;
+use YamlStandards\Model\Component\YamlService;
+use YamlStandards\Model\Config\StandardParametersData;
+use YamlStandards\Model\FixerInterface;
+use YamlStandards\Result\Result;
+
+/**
+ * Fix service file has uniform aliasing
+ */
+class YamlServiceAliasingFixer implements FixerInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function fix(string $pathToYamlFile, string $pathToDumpFixedFile, StandardParametersData $standardParametersData): Result
+    {
+        $yamlContent = file_get_contents($pathToYamlFile);
+        $yamlContent = str_replace("\r", '', $yamlContent); // remove carriage returns
+        $yamlLines = explode("\n", $yamlContent);
+
+        if (YamlServiceAliasingDataFactory::existsServicesInHighestParent($yamlLines) === false) {
+            return new Result($pathToYamlFile, Result::RESULT_CODE_OK, ProcessOutput::STATUS_CODE_OK);
+        }
+
+        $correctYamlLines = YamlServiceAliasingDataFactory::getCorrectYamlLines($yamlLines, YamlService::getYamlData($pathToYamlFile), $standardParametersData);
+        $correctYamlContent = implode("\n", $correctYamlLines);
+
+        if ($yamlContent === $correctYamlContent) {
+            return new Result($pathToYamlFile, Result::RESULT_CODE_OK, ProcessOutput::STATUS_CODE_OK);
+        }
+
+        file_put_contents($pathToDumpFixedFile, $correctYamlContent);
+
+        $differ = new Differ();
+        $diffBetweenStrings = $differ->diff($yamlContent, $correctYamlContent);
+
+        return new Result($pathToYamlFile, Result::RESULT_CODE_OK, ProcessOutput::STATUS_CODE_INVALID_FILE_SYNTAX, $diffBetweenStrings);
+    }
+}
