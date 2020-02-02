23069a76248d35879be8beeb65b65aebe4f85962
Peter Turek <peter.turek13@gmail.com>
2020-02-02 16:50:17 +0100

    Yaml inline: create tests (#10)

---
+++ tests/Model/YamlInline/YamlInlineCheckerTest.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace YamlStandards\Model\YamlInline;
+
+use PHPUnit\Framework\TestCase;
+use YamlStandards\Model\Config\StandardParametersData;
+use YamlStandards\Model\Config\YamlStandardConfigDefinition;
+use YamlStandards\Result\Result;
+
+class YamlInlineCheckerTest extends TestCase
+{
+    public function testCheckUnfixedFilesIsNotCorrect(): void
+    {
+        $pathToFiles = [
+            __DIR__ . '/resource/unfixed/symfony-config.yml',
+            __DIR__ . '/resource/unfixed/symfony-route.yml',
+            __DIR__ . '/resource/unfixed/symfony-security.yml',
+            __DIR__ . '/resource/unfixed/symfony-service.yml',
+            __DIR__ . '/resource/unfixed/version.yaml',
+            __DIR__ . '/resource/unfixed/yaml-getting-started.yml',
+        ];
+        $yamlInlineChecker = new YamlInlineChecker();
+
+        foreach ($pathToFiles as $pathToFile) {
+            $standardParametersData = $this->getStandardsParametersData();
+            $result = $yamlInlineChecker->check($pathToFile, $standardParametersData);
+
+            $this->assertSame(Result::RESULT_CODE_INVALID_FILE_SYNTAX, $result->getResultCode());
+        }
+    }
+
+    public function testCheckFixedFilesIsCorrect(): void
+    {
+        $pathToFiles = [
+            __DIR__ . '/resource/fixed/symfony-config.yml',
+            __DIR__ . '/resource/fixed/symfony-route.yml',
+            __DIR__ . '/resource/fixed/symfony-security.yml',
+            __DIR__ . '/resource/fixed/symfony-service.yml',
+            __DIR__ . '/resource/fixed/version.yaml',
+            __DIR__ . '/resource/fixed/yaml-getting-started.yml',
+        ];
+        $yamlInlineChecker = new YamlInlineChecker();
+
+        foreach ($pathToFiles as $pathToFile) {
+            $standardParametersData = $this->getStandardsParametersData();
+            $result = $yamlInlineChecker->check($pathToFile, $standardParametersData);
+
+            $this->assertSame(Result::RESULT_CODE_OK, $result->getResultCode(), sprintf('YAML inline check of "%s" failed.', $pathToFile));
+        }
+    }
+
+    /**
+     * @return \YamlStandards\Model\Config\StandardParametersData
+     */
+    private function getStandardsParametersData(): StandardParametersData
+    {
+        return new StandardParametersData(4, 4, 2, YamlStandardConfigDefinition::CONFIG_PARAMETERS_SERVICE_ALIASING_TYPE_VALUE_SHORT, YamlStandardConfigDefinition::CONFIG_PARAMETERS_INDENTS_COMMENTS_WITHOUT_PARENT_VALUE_DEFAULT);
+    }
+}
